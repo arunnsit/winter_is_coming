4c50206fd284008854219222494238e528c57182
arun <arun.yad96@gmail.com>
2017-03-21 01:01:58 +0530

    added to name to user , second way

--- food_net/db/schema.rb
+++ food_net/db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20170320185234) do
 
   create_table "restaurants", force: :cascade do |t|
@@ -20,6 +19,7 @@ ActiveRecord::Schema.define(version: 20170320185234) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string   "name",                   default: "", null: false
     t.string   "email",                  default: "", null: false
     t.string   "encrypted_password",     default: "", null: false
     t.string   "reset_password_token"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 20170320185234) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
-    t.string   "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
