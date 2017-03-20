d33c2d9d641bf2a5fdbe41354ae136530e2b460d
Deepanshu <deepanshupandey95@gmail.com>
2017-03-21 01:43:17 +0530

    Added attributes to restaurant model

--- food_net/db/migrate/20170320185234_create_restaurants.rb
+++ food_net/db/migrate/20170320185234_create_restaurants.rb
@@ -3,6 +3,10 @@ class CreateRestaurants < ActiveRecord::Migration[5.0]
     create_table :restaurants do |t|
     	t.string :name, null:false, default: ""
       t.timestamps null: false
+      t.string :address, null:false, default: ""
+      t.string :timings, null:false, default: ""
+      #image
+      t.integer :total_tables, null:false ,default: 0
     end
   end
 end
